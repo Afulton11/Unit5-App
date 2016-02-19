3432227d72b6e2a464353d4e68c3275416e0f683
Afulton11 <afulton20@gmail.com>
2016-02-19 16:06:25 -0600

    Added an enum 'EvenType' to the Calendar Event. This specifies the type of event the calendar event is. For example it might be a holiday, or just a regular day/event.

--- app/src/main/java/com/unit5app/com/unit5app/parsers/CalendarEvent.java
+++ app/src/main/java/com/unit5app/com/unit5app/parsers/CalendarEvent.java
@@ -2,18 +2,24 @@ package com.unit5app.com.unit5app.parsers;
 
 /**
  * Created by Andrew on 2/18/2016.
+ * This holds all the information needed for each calendar event on a calendar date.
  */
 public class CalendarEvent {
 
     private String date, timeOccurring, title;
+    private EventType type;
 
-    public CalendarEvent(String title, String date, String timeOccurring) {
+    private enum EventType {regular, holiday,};
+
+    public CalendarEvent(String title, String date, String timeOccurring, EventType type) {
         this.title = title;
         this.date = date;
         this.timeOccurring = timeOccurring;
+        this.type = type;
     }
 
     public String getDate() { return  date; }
     public String getTitle() { return  title; }
     public String getTimeOccurring() { return timeOccurring; }
+    public EventType getType() { return type; }
 }
