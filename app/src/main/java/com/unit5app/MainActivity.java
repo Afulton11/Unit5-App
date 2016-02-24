cadba6811cf805446fd45b4a7e8f17ed629d8184
BZ <ben01189998819991197253@users.noreply.github.com>
2016-02-24 09:26:06 -0600

    PDFReader class

--- app/src/main/java/com/unit5app/MainActivity.java
+++ app/src/main/java/com/unit5app/MainActivity.java
@@ -12,7 +12,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.unit5app.com.unit5app.parsers.PDFReader;
 import com.unit5app.utils.Utils;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,8 +67,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         /* TODO: Launch internal calendar builder to get latest info on events, etc... */
-        /*
-        The calendar reader is loaded up in here so that we will know what is happening on the day the app is opened. It may be a late start day.
+        /* TEST CODE, PROOF OF CONCEPT */
+        PDDocument doc = PDFReader.pullPDF("http://www.unit5.org/cms/lib03/IL01905100/Centricity/Domain/55/2016%20Feb%20Sr%20High%20Lunch.pdf");
+
+        /* The calendar reader is loaded up in here so that we will know what is happening on the day the app is opened. It may be a late start day.
          */
         if(Utils.isInternetConnected(getApplicationContext())) {
             UpcomingEventsActivity.loadCalendar();
