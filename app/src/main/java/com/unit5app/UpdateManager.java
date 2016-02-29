cce40d0c5f8f516eed6c68f4f5532332887d4150
BZ <ben01189998819991197253@users.noreply.github.com>
2016-02-29 09:33:17 -0600

    Update task definition

--- app/src/main/java/com/unit5app/UpdateManager.java
+++ app/src/main/java/com/unit5app/UpdateManager.java
@@ -1,12 +1,28 @@
 package com.unit5app;
 
+import android.os.AsyncTask;
+
 /**
  * @author Andrew and Ben
  * @version 2/26/16
  */
 public class UpdateManager{
+    private int updateTime; // determined by settings menu, (default 30min)
 
-    public UpdateManager() {
+    public UpdateManager(Object... objects) {
+        new QueryUpdatesTask().execute(objects);
     }
 
+    private class QueryUpdatesTask extends AsyncTask<Object, Void, Void> {
+        @Override
+        protected Void doInBackground(Object... objects) {
+            /* While true do updates every updateTime */
+            while (true) {
+                for(Object o : objects) {
+                    // Trigger updates
+                }
+                // Wait updateTime
+            }
+        }
+    }
 }
