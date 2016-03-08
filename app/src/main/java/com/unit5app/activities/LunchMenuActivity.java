cbef721c7aa5bc7feb362dab3f79759006090097
ben01189998819991197253 <ben01189998819991197253@users.noreply.github.com>
2016-03-08 09:50:43 -0600

    Lunchmenu work in progress

--- app/src/main/java/com/unit5app/activities/LunchMenuActivity.java
+++ app/src/main/java/com/unit5app/activities/LunchMenuActivity.java
@@ -1,6 +1,7 @@
 package com.unit5app.activities;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
 import android.webkit.WebView;
 
 import com.sun.pdfview.PDFImage;
@@ -10,12 +11,16 @@ import com.unit5app.R;
 
 import net.sf.andpdf.refs.HardReference;
 
+import java.io.File;
+import java.net.URL;
+
 /**
  * Created by z on 3/8/16.
  */
 public class LunchMenuActivity extends BaseActivity {
     private static final String TAG = "LunchMenuPDFReader";
     private WebView webView;
+    private int viewSize = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +35,21 @@ public class LunchMenuActivity extends BaseActivity {
         webView = (WebView)findViewById(R.id.lunchmenu_pdf);
         webView.getSettings().setBuiltInZoomControls(true); // Enable zoom buttons
         webView.getSettings().setSupportZoom(true);     // Make zoom work
+
+        webView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+                viewSize = webView.getWidth();
+                webView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            }
+        });
+
+        try {
+            //File file = new File(String.valueOf(new URL("http://www.unit5.org/cms/lib03/IL01905100/Centricity/Domain/55/2016%20Mar%20Sr%20High%20Lunch.pdf")));
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
