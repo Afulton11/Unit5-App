493d9301796802a86430a4d1d165b337daa51c5d
ben01189998819991197253 <ben01189998819991197253@users.noreply.github.com>
2016-03-14 09:56:49 -0500

    PDFs still can't display, doesn't crash hard anymore

--- app/src/main/java/com/unit5app/activities/LunchMenuActivity.java
+++ app/src/main/java/com/unit5app/activities/LunchMenuActivity.java
@@ -7,15 +7,25 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.widget.Toast;
 
+import com.unit5app.R;
+import com.unit5app.tasks.DownloadFile;
+import com.unit5app.utils.Utils;
+
 import java.io.File;
 
 public class LunchMenuActivity extends BaseActivity {
     private static final String TAG = "LunchMenuPDFReader";
+    private static final String fileUrl = "http://www.unit5.org/cms/lib03/IL01905100/Centricity/Domain/55/2016%20Mar%20Sr%20High%20Lunch.pdf";
     private static final String fileName = "2016 Mar Sr High Lunch.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        new DownloadFile().execute(fileUrl, fileName);
+
+        setContentView(R.layout.announcements_layout);
+        Utils.setCurrentView(Utils.VIEW_ANNOUNCEMENTS);
 
         File pdfFile = new File(Environment.getExternalStorageDirectory() + "/pdfCache/" + fileName);  // -> filename = thepdf.pdf
         Uri path = Uri.fromFile(pdfFile);
--- app/src/main/java/com/unit5app/tasks/DownloadFile.java
+++ app/src/main/java/com/unit5app/tasks/DownloadFile.java
@@ -2,6 +2,7 @@ package com.unit5app.tasks;
 
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,11 +13,37 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+public class DownloadFile extends AsyncTask<String, Void, Void> {
+    private static String TAG = "FileDownloader";
+
+    @Override
+    protected Void doInBackground(String... strings) {
+        String fileUrl = strings[0];   // http://unit5.org/directory/thepdf.pdf
+        String fileName = strings[1];  // thepdf.pdf
+        String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+        File folder = new File(extStorageDirectory, "pdfCache");
+        folder.mkdir();
+
+        File pdfFile = new File(folder, fileName);
+        Log.d(TAG, "Attempting to create file " + pdfFile.toString());
+
+        try {
+            pdfFile.createNewFile();
+            pdfFile.setReadable(true);
+            pdfFile.setWritable(true);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileDownloader.downloadFile(fileUrl, pdfFile);
+        return null;
+    }
+}
+
 /**
  * Sourced from http://stackoverflow.com/questions/24740228/android-download-pdf-from-url-then-open-it-with-a-pdf-reader
  */
-public class FileDownloader {
-    //private static String TAG = "FileDownloader";
+class FileDownloader {
     private static final int MEGABYTE = 1024*1024;
 
     public static void downloadFile(String fileUrl, File directory) {
@@ -37,33 +64,12 @@ public class FileDownloader {
             fileOutputStream.close();
         }
         catch (FileNotFoundException | MalformedURLException e) {
+            e.toString();
             e.printStackTrace();
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-    }
-}
-
-class DownloadFile extends AsyncTask<String, Void, Void> {
-    @Override
-    protected Void doInBackground(String... strings) {
-        String fileUrl = strings[0];   // http://unit5.org/directory/thepdf.pdf
-        String fileName = strings[1];  // thepdf.pdf
-        String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
-        File folder = new File(extStorageDirectory, "pdfCache");
-        folder.mkdir();
-
-        File pdfFile = new File(folder, fileName);
-
-        try {
-            pdfFile.createNewFile();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        FileDownloader.downloadFile(fileUrl, pdfFile);
-        return null;
     }
 }
 
